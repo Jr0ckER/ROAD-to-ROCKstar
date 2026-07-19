70650db19279240f05513945061a980aace845a0
Jr0ckER <papipayo65@gmail.com>
2026-07-20 08:21:38 +0900

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,73 @@
+const questions = [
+
+{
+    question:"Saucy Dogのボーカルは？",
+    choices:["石原慎也","野田洋次郎","尾崎世界観","藤原基央"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"SHISHAMOの出身地は？",
+    choices:["神奈川県","大阪府","東京都","北海道"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"RADWIMPSのボーカルは？",
+    choices:["野田洋次郎","石原慎也","川谷絵音","後藤正文"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"BUMP OF CHICKENのボーカルは？",
+    choices:["藤原基央","野田洋次郎","山内総一郎","尾崎世界観"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"ヨルシカのコンポーザーは？",
+    choices:["n-buna","ACAね","川谷絵音","TK"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"ずっと真夜中でいいのに。のボーカル名義は？",
+    choices:["ACAね","suis","ikura","milet"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"My Hair is Badの出身地は？",
+    choices:["新潟県","徳島県","大阪府","東京都"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"マカロニえんぴつのボーカルは？",
+    choices:["はっとり","石原慎也","尾崎世界観","北村匠海"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"クリープハイプのボーカルは？",
+    choices:["尾崎世界観","川谷絵音","野田洋次郎","藤原基央"],
+    answer:0,
+    reward:20
+},
+
+{
+    question:"ASIAN KUNG-FU GENERATIONのボーカルは？",
+    choices:["後藤正文","川上洋平","細美武士","金井政人"],
+    answer:0,
+    reward:20
+}
+
+];
